0ce360dd05f56ac9c6ff9ea11153b0de656a53e5
Valery Guhena <valeryghn10@outlook.com>
2022-11-06 22:43:03 +0000

    Nov 7, 2022

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import { AppHeader } from "./components/AppHeader";
+import { ForgotPassword } from "./pages/ForgotPassword";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Offers } from "./pages/Offers";
+import { Profile } from "./pages/Profile";
+import { Register } from "./pages/Register";
 
 
 function App() {
   return (
-    <div className="text-2xl bg-teal-800 text-white">
-      <h2>Valery Guhena</h2>
-    </div>
+    <>
+      <AppHeader />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </>
   );
 }
 
